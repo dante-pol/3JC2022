9efbc032eb31152e06e238dbecf186a6b297efd8
Dante Pol <boss@sarosgames.ru>
2022-09-30 20:32:53 +0300

    Add generator containers.

--- Assets/GeneratorContainers.cs
+++ Assets/GeneratorContainers.cs
@@ -1,19 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GeneratorContainers : MonoBehaviour
 {
+    #region Data
+    [SerializeField] [Range(1, 10)] private int offsetBetweenContainers;
+    #endregion
 
+    [SerializeField] private GameObject[] _prefabs;
+    [SerializeField] private Transform _parent;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        int currentPosition = offsetBetweenContainers;
+        for (int i = 1; i <= 10; i++)
+        {
+            var item = Instantiate(_prefabs[Random.Range(0, _prefabs.Length)], _parent);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            item.transform.position = new Vector3(0, i * offsetBetweenContainers, 0);
+        }
     }
 }
