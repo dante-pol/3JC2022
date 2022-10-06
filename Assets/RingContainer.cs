a43e958127215d99e667a1d7647b5f6d630f3e07
Dante Pol <boss@sarosgames.ru>
2022-10-06 13:31:19 +0300

    Added unenabled collider for ring. Added random rotate for ring.

--- Assets/RingContainer.cs
+++ Assets/RingContainer.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
+[RequireComponent(typeof(BoxCollider))]
 public class RingContainer : MonoBehaviour
 {
     [SerializeField] private ObjectExplosion[] _elements;
     private float _explosionForce;
     private float _explosionRadius;
     private BallJump _ball;
+    private BoxCollider _collider;
 
     public void Inisialization(BallJump ball, float explosionForce, float explosionRadius)
     {
         _ball = ball;
         _explosionForce = explosionForce;
         _explosionRadius = explosionRadius;
+        _collider = GetComponent<BoxCollider>();
+        transform.Rotate(Vector3.up, Random.Range(-20, 20));
     }
 
     public void RunExplosion()
     {
+        _collider.enabled = false;
         foreach(ObjectExplosion element in _elements)
         {
             element.Explosion(_explosionForce, _explosionRadius, transform.position);
